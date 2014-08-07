7b67acb427944e377fd1cebc7c090636aec94f34
markusgirdland <mg222pi@student.lnu.se>
2014-08-07 08:32:31 +0000

    mobilversion & uppdatering

---
+++ script/script.js
@@ -0,0 +1,33 @@
+var myApp = {
+
+    init: function () {
+        
+        console.log("hej");
+        
+        // Create the dropdown base
+        $("<select />").appendTo("nav");
+        
+        // Create default option "Go to..."
+        $("<option />", {
+           "selected": "selected",
+           "value"   : "",
+           "text"    : "Go to..."
+        }).appendTo("nav select");
+        
+        // Populate dropdown with menu items
+        $("nav a").each(function() {
+         var el = $(this);
+         $("<option />", {
+             "value"   : el.attr("href"),
+             "text"    : el.text()
+         }).appendTo("nav select");
+        });
+        
+        $("nav select").change(function() {
+          window.location = $(this).find("option:selected").val();
+        });
+    
+    }
+};
+
+window.onload = myApp.init;
